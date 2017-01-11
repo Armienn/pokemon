0d0f4a2e49b2826f8fdf1a9919e3adcea9f1ddac
Nequilich <nequilich@gmail.com>
2017-01-11 21:03:24 +0100

    Did more stuff

--- static/script.js
+++ static/script.js
@@ -1,23 +1,60 @@
+var filterlist = document.getElementById("filter-list")
+var currentfilterlist = document.getElementById("current-filter-list")
 var pokelist = document.getElementById("pokemon-list")
 var pokeinfo = document.getElementById("pokemon-info")
 var pokemons = []
 var moves = []
+var filters = []
 
 function update(){
-	var pokes = pokemons.filter(hasMoveAmongFilter("tackle"))
-	pokes = pokes.filter(hasAbilityAmongFilter("intimidate"))
+	var pokes = pokemons
+	clearInterface()
+	for(var i in filters){
+		pokes = pokes.filter(filters[i])
+	}
 	for(var i in pokes){
 		pokelist.appendChild(createPokemonListElement(pokes[i]))
 	}
+}
+
+function clearInterface(){
+	while (pokelist.firstChild)
+		pokelist.removeChild(pokelist.firstChild)
+	while (currentfilterlist.firstChild)
+		currentfilterlist.removeChild(currentfilterlist.firstChild)
+}
+
+function addFilterEntry(label, filterFunction){
+	var filterElement = newTag("li", filterlist)
+	var labelElement = newTag("label", filterElement)
+	labelElement.innerHTML = label
+	var inputElement = newTag("input", filterElement)
+	var addElement = newTag("button", filterElement)
+	addElement.innerHTML = "Add"
+	addElement.onclick = function(){
+		addFilter(filterFunction, inputElement.value)
+		update()
+	}
+}
+
+function addFilter(filterFunction, input){
+	var inputs = input.split(",")
+	for(var i in inputs)
+		inputs[i] = inputs[i].trim()
+	filters.push(filterFunction(inputs))
 }
 
 function createPokemonListElement(pokemon) {
 	var pokeElement = newTag("li")
 	pokeElement.innerHTML = (pokemon.form == "Base" ? "" : pokemon.form + " " ) + pokemon.name
 	pokeElement.onclick = function(){
-		pokeinfo.innerHTML = JSON.stringify(pokemon)
+		updatePokemonInfo(pokemon)
 	}
 	return pokeElement
+}
+
+function updatePokemonInfo(pokemon){
+	pokeinfo.innerHTML = JSON.stringify(pokemon)
 }
 
 function textifyPokemons(pokemons){
@@ -78,5 +115,7 @@ function getPokemons(response){
 		update()
 }
 
-request("https://armienn.github.io/pokemon/static/moves.json", getMoves);
-request("https://armienn.github.io/pokemon/static/pokemons-small.json", getPokemons);
+request("https://armienn.github.io/pokemon/static/moves.json", getMoves)
+request("https://armienn.github.io/pokemon/static/pokemons-small.json", getPokemons)
+addFilterEntry("Move filter:", hasMoveAmongFilter)
+addFilterEntry("Ability filter:", hasAbilityAmongFilter)
