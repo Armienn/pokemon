eab48e134421f75a91b80d5634dee4e4d585eacd
Armienn <nigathr@gmail.com>
2017-01-11 16:07:03 +0100

    Mulle

--- static/script.js
+++ static/script.js
@@ -5,7 +5,26 @@ var pokemons = []
 var moves = []
 
 function update(){
-    element.innerHTML = JSON.stringify(moves)
+    for(var i in pokemons){
+        var pokemon = pokemons[i]
+        if(pokemon.name == pokemon.form)
+            pokemon.form = "Base"
+        else if(pokemon.form.startsWith("Mega")) {
+            pokemon.form = "Mega"
+            if(pokemon.form.endsWith("X"))
+                pokemon.form += " X"
+            if(pokemon.form.endsWith("Y"))
+                pokemon.form += " Y"
+        }
+        else if(pokemon.form.startsWith("Alolan")) {
+            pokemon.form = "Alola"
+        }
+    }
+    var result = "[<br>"
+    for(var i in pokemons){
+        result += JSON.stringify(pokemons[i]) + ",<br>"
+    }
+    element.innerHTML = result + "]"
 }
 
 function request(url, callback) {
@@ -20,11 +39,15 @@ function request(url, callback) {
 
 function getMoves(response){
     moves = JSON.parse(response)
-    update()
+    if(moves && pokemons)
+        update()
 }
 
-function getPokemon(response){
+function getPokemons(response){
     pokemons = JSON.parse(response)
+    if(moves && pokemons)
+        update()
 }
 
 request("https://armienn.github.io/pokemon/static/moves.json", getMoves);
+request("https://armienn.github.io/pokemon/static/pokemons-small.json", getPokemons);
