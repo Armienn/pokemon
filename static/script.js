12153e74e3f22a98ace984da722c11799be2e395
Nequilich <nequilich@gmail.com>
2017-01-11 19:13:30 +0100

    Added filter functions

--- static/script.js
+++ static/script.js
@@ -5,42 +5,56 @@ var pokemons = []
 var moves = []
 
 function update(){
-    var pokesA = moves["False Swipe"].pokemons
-    var pokesB = moves["Spore"].pokemons
-    var pokes = []
-    for(var i in pokesA){
-        for(var j in pokesB){
-            if(pokesA[i].id == pokesB[j].id && pokesA[i].form == pokesB[j].form)
-            pokes.push(pokesA[i])
-        }
-    }
-    var result = ""
-    for(var i in pokes){
-        result += JSON.stringify(pokes[i]) + ",<br>"
-    }
-    element.innerHTML = result
+	var pokes = pokemons.filter(hasMoveAmongFilter("tackle"))
+	pokes = pokes.filter(hasAbilityAmongFilter("intimidate"))
+	var result = ""
+	for(var i in pokes){
+		result += (pokes[i].form == "Base" ? "" : pokes[i].form + " " ) + pokes[i].name + "<br>"
+		//result += JSON.stringify(pokes[i]) + "<br>"
+	}
+	element.innerHTML = result
+}
+
+function hasMoveAmongFilter(...moves) {
+	return function(pokemon) {
+		for(var i in moves){
+			if(pokemon.moves.filter(e => e.name.toLowerCase() == moves[i].toLowerCase()).length)
+				return true
+		}
+		return false
+	}
+}
+
+function hasAbilityAmongFilter(...abilities) {
+	return function(pokemon) {
+		for(var i in abilities){
+			if(pokemon.abilities.filter(e => e ? e.toLowerCase() == abilities[i].toLowerCase() : false).length)
+				return true
+		}
+		return false
+	}
 }
 
 function request(url, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText)
-    }
-    xmlHttp.open("GET", url, true)
-    xmlHttp.send()
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() { 
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			callback(xmlHttp.responseText)
+	}
+	xmlHttp.open("GET", url, true)
+	xmlHttp.send()
 }
 
 function getMoves(response){
-    moves = JSON.parse(response)
-    if(moves && pokemons)
-        update()
+	moves = JSON.parse(response)
+	if(moves && pokemons)
+		update()
 }
 
 function getPokemons(response){
-    pokemons = JSON.parse(response)
-    if(moves && pokemons)
-        update()
+	pokemons = JSON.parse(response)
+	if(moves && pokemons)
+		update()
 }
 
 request("https://armienn.github.io/pokemon/static/moves.json", getMoves);
