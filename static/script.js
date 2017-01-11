b4fcee910447d2337ce1932da3eea865efc02880
Armienn <nigathr@gmail.com>
2017-01-11 15:53:03 +0100

    Trying stuff

--- static/script.js
+++ static/script.js
@@ -4,8 +4,27 @@ element.innerHTML = "Hej du"
 var pokemons = []
 var moves = []
 
-
-
 function update(){
     element.innerHTML = JSON.stringify(moves)
 }
+
+function request(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText)
+    }
+    xmlHttp.open("GET", url, true)
+    xmlHttp.send()
+}
+
+function getMoves(response){
+    moves = JSON.parse(response)
+    update()
+}
+
+function getPokemon(response){
+    pokemons = JSON.parse(response)
+}
+
+request("https://armienn.github.io/pokemon/static/moves.json", getMoves);
