7b258321b078d4cd1a2efafe1a9908d5d47c7c8c
Armienn <nigathr@gmail.com>
2017-01-13 21:25:20 +0100

    Added ugly bars

--- static/layout-info.js
+++ static/layout-info.js
@@ -62,15 +62,84 @@ function updatePokemonInfo(pokemon){
 }
 
 function showPokemonInfo(pokemon){
+	clearPokemonInfo()
+	showNameHeader(pokemon)
+	showImageSection(pokemon)
+	showInfoSection(pokemon)
+	showStatSection(pokemon)
+	showMovesSection(pokemon)
+}
+
+function showNameHeader(pokemon){
 	nameHeader.innerHTML = "#" + pokemon.id + " - " + pokemon.name
 	if(pokemon.form && pokemon.form != "Base")
 		nameHeader.innerHTML += " (" + pokemon.form + ")"
 	var colorA = typeColors[pokemon.types[0]]
 	var colorB = pokemon.types[1] ? typeColors[pokemon.types[1]] : typeColors[pokemon.types[0]]
 	nameHeader.style.background = "linear-gradient(to right, " + colorA + ", " + colorB + ")"
+}
+
+function showImageSection(pokemon){
 	imageSection.innerHTML = "<img src='https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular/" + pokemonSimpleName(pokemon) + ".png'/>"
-	infoSection.innerHTML = getTypesText(pokemon)
-	statSection.innerHTML = JSON.stringify(pokemon.stats)
+}
+
+function showInfoSection(pokemon){
+	var element = newTag("li", infoSection)
+	element.innerHTML = getTypesText(pokemon)
+	element = newTag("li", infoSection)
+	element.innerHTML = pokemon.classification
+	element = newTag("li", infoSection)
+	element.innerHTML = pokemon.ratio
+	element = newTag("li", infoSection)
+	element.innerHTML = pokemon.eggCycles
+}
+
+function showStatSection(pokemon){
+	var element = statSection.children[0].children[1]
+	element.appendChild(getStatElement(pokemon, "hp", "HP"))
+	element.appendChild(getStatElement(pokemon, "atk", "Attack"))
+	element.appendChild(getStatElement(pokemon, "def", "Defense"))
+	element.appendChild(getStatElement(pokemon, "spa", "Sp. Atk"))
+	element.appendChild(getStatElement(pokemon, "spd", "Sp. Def"))
+	element.appendChild(getStatElement(pokemon, "spe", "Speed"))
+}
+
+function getStatElement(pokemon, stat, headerText){
+	var row = newTag("tr")
+	var header = newTag("th", row)
+	var text = newTag("td", row)
+	var barElement = newTag("td", row)
+	header.innerHTML = headerText
+	var statBase = pokemon.stats[stat]
+	var ivBase = pokemon.ivs ? pokemon.ivs[stat] : 0
+	var evBase = pokemon.evs ? pokemon.evs[stat] : 0
+	text.innerHTML = statBase
+	if(pokemon.ivs || pokemon.evs)
+		text.innerHTML += " · " + ivBase + " · " + evBase
+	var bar = newTag("div", barElement)
+	bar.className = "stat-bar"
+	bar.style.backgroundColor = "yellow"
+	bar.style.width = statBase*2 + "px"
+	bar = newTag("div", barElement)
+	bar.className = "stat-bar"
+	bar.style.backgroundColor = "orange"
+	bar.style.width = ivBase*2 + "px"
+	bar = newTag("div", barElement)
+	bar.className = "stat-bar"
+	bar.style.backgroundColor = "red"
+	bar.style.width = evBase*2 + "px"
+	return row
+}
+
+function showMovesSection(pokemon){
+	
+}
+
+function clearPokemonInfo(){
+	while (statSection.children[0].children[1].firstChild)
+		statSection.children[0].children[1].removeChild(statSection.children[0].children[1].firstChild)
+	while (infoSection.firstChild)
+		infoSection.removeChild(infoSection.firstChild)
 }
 
 function getTypeText(type){
