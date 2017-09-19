1bbdcf913f0e28e65f398017a39361ab0f0a90f5
Armienn <nigathr@gmail.com>
2017-09-19 10:43:23 +0200

    Refactored some of spreadsheet parsers functionality

--- static/porting.js
+++ static/porting.js
@@ -20,31 +20,31 @@ class Porting {
 			pokemons.push(new Pokemon(list[n]))
 		return pokemons
 	}
-	
+
 	static importSmogon(input) {
 		var pokemons = []
 		var list = input.trim().split("\n\n")
 		for (var n in list)
-			if(list[n].trim().length)
+			if (list[n].trim().length)
 				pokemons.push(Porting.parseSmogonPokemon(list[n]))
 		return pokemons
 	}
-	
+
 	static parseSmogonPokemon(input) {
 		var parts = input.trim().split("\n")
-		var pokemon = new Pokemon({name:parts[0].split("@")[0].trim()})
+		var pokemon = new Pokemon({ name: parts[0].split("@")[0].trim() })
 		pokemon.ability = parts[1].split(":")[1].trim()
 		var evParts = parts[2].split(":")[1].split("/")
-		pokemon.ivs = {hp:"31",atk:"31",def:"31",spa:"31",spd:"31",spe:"31"}
-		pokemon.evs = {hp:"0",atk:"0",def:"0",spa:"0",spd:"0",spe:"0"}
-		for(var i in evParts){
-				var ev = evParts[i].trim().split(" ")
-				pokemon.evs[ev[1].toLowerCase()] = ev[0]
+		pokemon.ivs = { hp: "31", atk: "31", def: "31", spa: "31", spd: "31", spe: "31" }
+		pokemon.evs = { hp: "0", atk: "0", def: "0", spa: "0", spd: "0", spe: "0" }
+		for (var i in evParts) {
+			var ev = evParts[i].trim().split(" ")
+			pokemon.evs[ev[1].toLowerCase()] = ev[0]
 		}
 		pokemon.nature = parts[3].trim().split(" ")[0]
 		pokemon.learntMoves = []
-		for(var i=4;i<parts.length && i<8;i++)
-				pokemon.learntMoves.push(parts[i].split("-")[1].trim())
+		for (var i = 4; i < parts.length && i < 8; i++)
+			pokemon.learntMoves.push(parts[i].split("-")[1].trim())
 		return pokemon
 	}
 
@@ -76,5 +76,156 @@ class Porting {
 			table += "\n"
 		}
 		return table
+	}
+
+	static parseTable(table) {
+		var pokemons = []
+		for (var i = 1; i < table.length; i++) {
+			var pokemon = Porting.parsePokemonTableEntry(table[i], table[0])
+			if (pokemon)
+				pokemons.push(pokemon)
+		}
+		return pokemons
+	}
+
+	static find(entry, headers, values) {
+		for (var i in values)
+			for (var j in headers)
+				if (headers[j] == values[i])
+					return typeof entry[j] == "string" ? entry[j].trim() : entry[j]
+	}
+
+	static parsePokemonTableEntry(entry, headers) {
+		var id = Number(Porting.find(entry, headers, ["dexno", "no", "number", "id"]))
+		var name = Porting.find(entry, headers, ["pokemon", "name"])
+		var form = Porting.find(entry, headers, ["form"])
+		if (name) {
+			var splitName = name.split("(")
+			if (1 < splitName.length) {
+				name = splitName[0].trim()
+				if (!form)
+					form = splitName[1].split(")")[0].trim()
+			}
+		}
+		var pokemon = stuff.data.getPokemonFrom({ name: name, id: id, form: form })
+		if (!pokemon)
+			return
+		pokemon.nature = Porting.find(entry, headers, ["nature"])
+		pokemon.ability = Porting.find(entry, headers, ["ability"])
+		pokemon.ivs = {}
+		pokemon.ivs.hp = Porting.find(entry, headers, ["hpiv", "ivhp", "hp"]) || "x"
+		pokemon.ivs.atk = Porting.find(entry, headers, ["atkiv", "attackiv", "attack", "ivattack", "ivatk", "atk"]) || "x"
+		pokemon.ivs.def = Porting.find(entry, headers, ["defiv", "defenseiv", "defense", "ivdefense", "ivdef", "def"]) || "x"
+		pokemon.ivs.spa = Porting.find(entry, headers, ["spaiv", "spatkiv", "spatk", "ivspatk", "ivspa", "spa"]) || "x"
+		pokemon.ivs.spd = Porting.find(entry, headers, ["spdiv", "spdefiv", "spdef", "ivspdef", "ivspd", "spd"]) || "x"
+		pokemon.ivs.spe = Porting.find(entry, headers, ["speiv", "speediv", "speed", "ivspeed", "ivspe", "spe"]) || "x"
+		pokemon.evs = {}
+		pokemon.evs.hp = Porting.find(entry, headers, ["hpev", "evhp"]) || "x"
+		pokemon.evs.atk = Porting.find(entry, headers, ["atkev", "attackev", "evattack", "evatk"]) || "x"
+		pokemon.evs.def = Porting.find(entry, headers, ["defev", "defenseev", "evdefense", "evdef"]) || "x"
+		pokemon.evs.spa = Porting.find(entry, headers, ["spaev", "spatkev", "evspatk", "evspa"]) || "x"
+		pokemon.evs.spd = Porting.find(entry, headers, ["spdev", "spdefev", "evspdef", "evspd"]) || "x"
+		pokemon.evs.spe = Porting.find(entry, headers, ["speev", "speedev", "evspeed", "evspe"]) || "x"
+		pokemon.hiddenPower = Porting.find(entry, headers, ["hiddenpower", "hidden"])
+		pokemon.learntMoves = [
+			Porting.find(entry, headers, ["move1", "eggmove1", "moveslot1"]),
+			Porting.find(entry, headers, ["move2", "eggmove2", "moveslot2"]),
+			Porting.find(entry, headers, ["move3", "eggmove3", "moveslot3"]),
+			Porting.find(entry, headers, ["move4", "eggmove4", "moveslot4"])
+		].filter(e => e)
+		pokemon.gender = Porting.find(entry, headers, ["gender", "sex", "mf", "fm"])
+		switch (pokemon.base.ratio) {
+			case "1:0":
+				pokemon.gender = '♂'
+				break
+			case "0:1":
+				pokemon.gender = '♀'
+				break
+			case "—":
+				pokemon.gender = '—'
+				break
+		}
+		if (pokemon.gender) {
+			if (pokemon.gender.indexOf("♂") > -1 || pokemon.gender.toLowerCase() == "m" || pokemon.gender.toLowerCase() == "male")
+				pokemon.gender = "♂"
+			if (pokemon.gender.indexOf("♀") > -1 || pokemon.gender.toLowerCase() == "f" || pokemon.gender.toLowerCase() == "female")
+				pokemon.gender = "♀"
+			if (pokemon.gender.toLowerCase() == "-" || pokemon.gender.toLowerCase() == "none")
+				pokemon.gender = '—'
+		}
+
+		pokemon.amount = Porting.find(entry, headers, ["amount", "count"])
+		pokemon.shiny = Porting.find(entry, headers, ["shiny"])
+		pokemon.nickname = Porting.find(entry, headers, ["nickname"])
+		pokemon.ot = Porting.find(entry, headers, ["ot"])
+		pokemon.tid = Porting.find(entry, headers, ["tid"])
+		pokemon.level = Porting.find(entry, headers, ["level", "lvl", "lv"])
+		pokemon.language = Porting.find(entry, headers, ["language", "lang"])
+		pokemon.notes = Porting.find(entry, headers, ["notes", "note", "comments", "comment"])
+		pokemon.balls = []
+		var balls = Porting.find(entry, headers, ["pokeball", "ball", "pokeballs", "balls"])
+		if (balls) {
+			balls = balls.split(",")
+			for (var i in balls)
+				balls[i] = balls[i].trim()
+			pokemon.balls = balls.filter(e => e)
+		}
+		if (pokemon.balls.length == 0) {
+			if (Porting.find(entry, headers, ["poke"])) pokemon.balls.push("Poké Ball")
+			if (Porting.find(entry, headers, ["great"])) pokemon.balls.push("Great Ball")
+			if (Porting.find(entry, headers, ["ultra"])) pokemon.balls.push("Ultra Ball")
+			if (Porting.find(entry, headers, ["master"])) pokemon.balls.push("Master Ball")
+			if (Porting.find(entry, headers, ["safari"])) pokemon.balls.push("Safari Ball")
+			if (Porting.find(entry, headers, ["level"])) pokemon.balls.push("Level Ball")
+			if (Porting.find(entry, headers, ["lure"])) pokemon.balls.push("Lure Ball")
+			if (Porting.find(entry, headers, ["moon"])) pokemon.balls.push("Moon Ball")
+			if (Porting.find(entry, headers, ["friend"])) pokemon.balls.push("Friend Ball")
+			if (Porting.find(entry, headers, ["love"])) pokemon.balls.push("Love Ball")
+			if (Porting.find(entry, headers, ["heavy"])) pokemon.balls.push("Heavy Ball")
+			if (Porting.find(entry, headers, ["fast"])) pokemon.balls.push("Fast Ball")
+			if (Porting.find(entry, headers, ["sport"])) pokemon.balls.push("Sport Ball")
+			if (Porting.find(entry, headers, ["premier"])) pokemon.balls.push("Premier Ball")
+			if (Porting.find(entry, headers, ["repeat"])) pokemon.balls.push("Repeat Ball")
+			if (Porting.find(entry, headers, ["timer"])) pokemon.balls.push("Timer Ball")
+			if (Porting.find(entry, headers, ["nest"])) pokemon.balls.push("Nest Ball")
+			if (Porting.find(entry, headers, ["net"])) pokemon.balls.push("Net Ball")
+			if (Porting.find(entry, headers, ["dive"])) pokemon.balls.push("Dive Ball")
+			if (Porting.find(entry, headers, ["luxury"])) pokemon.balls.push("Luxury Ball")
+			if (Porting.find(entry, headers, ["heal"])) pokemon.balls.push("Heal Ball")
+			if (Porting.find(entry, headers, ["quick"])) pokemon.balls.push("Quick Ball")
+			if (Porting.find(entry, headers, ["dusk"])) pokemon.balls.push("Dusk Ball")
+			if (Porting.find(entry, headers, ["cherish"])) pokemon.balls.push("Cherish Ball")
+			if (Porting.find(entry, headers, ["dream"])) pokemon.balls.push("Dream Ball")
+			if (Porting.find(entry, headers, ["beast"])) pokemon.balls.push("Beast Ball")
+		}
+		if (pokemon.balls.length == 0) { // compatibility with richi3f's sheet
+			if (Porting.find(entry, headers, ["_dcgjs"])) pokemon.balls.push("Poké Ball")
+			if (Porting.find(entry, headers, ["_ddv49"])) pokemon.balls.push("Great Ball")
+			if (Porting.find(entry, headers, ["_d415a"])) pokemon.balls.push("Ultra Ball")
+			if (Porting.find(entry, headers, ["_d5fpr"])) pokemon.balls.push("Master Ball")
+			if (Porting.find(entry, headers, ["_d6ua4"])) pokemon.balls.push("Safari Ball")
+			if (Porting.find(entry, headers, ["_d88ul"])) pokemon.balls.push("Level Ball")
+			if (Porting.find(entry, headers, ["_dkvya"])) pokemon.balls.push("Lure Ball")
+			if (Porting.find(entry, headers, ["_dmair"])) pokemon.balls.push("Moon Ball")
+			if (Porting.find(entry, headers, ["_dnp34"])) pokemon.balls.push("Friend Ball")
+			if (Porting.find(entry, headers, ["_dp3nl"])) pokemon.balls.push("Love Ball")
+			if (Porting.find(entry, headers, ["_df9om"])) pokemon.balls.push("Heavy Ball")
+			if (Porting.find(entry, headers, ["_dgo93"])) pokemon.balls.push("Fast Ball")
+			if (Porting.find(entry, headers, ["_di2tg"])) pokemon.balls.push("Sport Ball")
+			if (Porting.find(entry, headers, ["_djhdx"])) pokemon.balls.push("Premier Ball")
+			if (Porting.find(entry, headers, ["_dw4je"])) pokemon.balls.push("Repeat Ball")
+			if (Porting.find(entry, headers, ["_dxj3v"])) pokemon.balls.push("Timer Ball")
+			if (Porting.find(entry, headers, ["_dyxo8"])) pokemon.balls.push("Nest Ball")
+			if (Porting.find(entry, headers, ["_e0c8p"])) pokemon.balls.push("Net Ball")
+			if (Porting.find(entry, headers, ["_dqi9q"])) pokemon.balls.push("Dive Ball")
+			if (Porting.find(entry, headers, ["_drwu7"])) pokemon.balls.push("Luxury Ball")
+			if (Porting.find(entry, headers, ["_dtbek"])) pokemon.balls.push("Heal Ball")
+			if (Porting.find(entry, headers, ["_dupz1"])) pokemon.balls.push("Quick Ball")
+			if (Porting.find(entry, headers, ["_e7d2q"])) pokemon.balls.push("Dusk Ball")
+			if (Porting.find(entry, headers, ["_e8rn7"])) pokemon.balls.push("Cherish Ball")
+			if (Porting.find(entry, headers, ["_ea67k"])) pokemon.balls.push("Dream Ball")
+			if (Porting.find(entry, headers, ["_ebks1"])) pokemon.balls.push("Beast Ball")
+		}
+		return pokemon
 	}
 }
--- static/spreadsheet-parser.js
+++ static/spreadsheet-parser.js
@@ -27,11 +27,11 @@ class SpreadsheetParser {
 	}
 
 	getWorksheetUrl(spreadsheetId, worksheetId) {
-		return "https://spreadsheets.google.com/feeds/list/" + spreadsheetId + "/" + worksheetId + "/public/values?alt=json";
+		return "https://spreadsheets.google.com/feeds/list/" + spreadsheetId + "/" + worksheetId + "/public/values?alt=json"
 	}
 
 	getSpreadsheetUrl(spreadsheetId) {
-		return "https://spreadsheets.google.com/feeds/worksheets/" + spreadsheetId + "/public/basic?alt=json";
+		return "https://spreadsheets.google.com/feeds/worksheets/" + spreadsheetId + "/public/basic?alt=json"
 	}
 
 	getValue(field) {
@@ -58,14 +58,34 @@ class SpreadsheetParser {
 
 	parseSheet(tab) {
 		return (response) => {
-			for (var i in response.feed.entry)
-				this.loadPokemon(response.feed.entry[i], tab)
+			var table = this.tablify(response.feed.entry)
+			tab.pokemons = Porting.parseTable(table)
 			if (tab.id == stuff.state.destination)
 				stuff.selectTab(tab)
 			stuff.state.externalInventory.tabsLoaded[tab.id] = true
 			this.updateExternalInventoryLoadedness()
 			stuff.tryLoad()
 		}
+	}
+
+	tablify(entries) {
+		var headers = {}
+		var table = [[]]
+		for (var i in entries) {
+			var entry = entries[i]
+			var newEntry = []
+			var keys = Object.keys(entry)
+			for (var j in keys) {
+				var key = keys[j].substr(4)
+				if (!(0 < headers[key]))
+					headers[key] = Object.keys(headers).length
+				newEntry[headers[key]] = entry[keys[j]].$t
+			}
+			table.push(newEntry)
+		}
+		for (var i in headers)
+			table[0][headers[i]] = i
+		return table
 	}
 
 	updateExternalInventoryLoadedness() {
@@ -103,139 +123,5 @@ class SpreadsheetParser {
 		stuff.state.externalInventory.tabsLoaded["config"] = true
 		this.updateExternalInventoryLoadedness()
 		stuff.tryLoad()
-	}
-
-	loadPokemon(entry, tab) {
-		var id = Number(this.tryValues(["dexno", "no", "number", "id"], entry))
-		var name = this.tryValues(["pokemon", "name"], entry)
-		var form = this.tryValues(["form"], entry)
-		if (name) {
-			var splitName = name.split("(")
-			if (1 < splitName.length) {
-				name = splitName[0].trim()
-				if (!form)
-					form = splitName[1].split(")")[0].trim()
-			}
-		}
-		var pokemon = stuff.data.getPokemonFrom({ name: name, id: id, form: form })
-		if (!pokemon)
-			return
-		pokemon.nature = this.getValue(entry.gsx$nature)
-		pokemon.ability = this.getValue(entry.gsx$ability)
-		pokemon.ivs = {}
-		pokemon.ivs.hp = this.tryValues(["hpiv", "ivhp", "hp"], entry) || "x"
-		pokemon.ivs.atk = this.tryValues(["atkiv", "attackiv", "attack", "ivattack", "ivatk", "atk"], entry) || "x"
-		pokemon.ivs.def = this.tryValues(["defiv", "defenseiv", "defense", "ivdefense", "ivdef", "def"], entry) || "x"
-		pokemon.ivs.spa = this.tryValues(["spaiv", "spatkiv", "spatk", "ivspatk", "ivspa", "spa"], entry) || "x"
-		pokemon.ivs.spd = this.tryValues(["spdiv", "spdefiv", "spdef", "ivspdef", "ivspd", "spd"], entry) || "x"
-		pokemon.ivs.spe = this.tryValues(["speiv", "speediv", "speed", "ivspeed", "ivspe", "spe"], entry) || "x"
-		pokemon.evs = {}
-		pokemon.evs.hp = this.tryValues(["hpev", "evhp"], entry) || "x"
-		pokemon.evs.atk = this.tryValues(["atkev", "attackev", "evattack", "evatk"], entry) || "x"
-		pokemon.evs.def = this.tryValues(["defev", "defenseev", "evdefense", "evdef"], entry) || "x"
-		pokemon.evs.spa = this.tryValues(["spaev", "spatkev", "evspatk", "evspa"], entry) || "x"
-		pokemon.evs.spd = this.tryValues(["spdev", "spdefev", "evspdef", "evspd"], entry) || "x"
-		pokemon.evs.spe = this.tryValues(["speev", "speedev", "evspeed", "evspe"], entry) || "x"
-		pokemon.hiddenPower = this.tryValues(["hiddenpower", "hidden"], entry)
-		pokemon.learntMoves = [
-			this.tryValues(["move1", "eggmove1", "moveslot1"], entry),
-			this.tryValues(["move2", "eggmove2", "moveslot2"], entry),
-			this.tryValues(["move3", "eggmove3", "moveslot3"], entry),
-			this.tryValues(["move4", "eggmove4", "moveslot4"], entry)
-		].filter(e => e)
-		pokemon.gender = this.tryValues(["gender", "sex", "mf", "fm"], entry)
-		switch (pokemon.base.ratio) {
-			case "1:0":
-				pokemon.gender = '♂'
-				break;
-			case "0:1":
-				pokemon.gender = '♀'
-				break;
-			case "—":
-				pokemon.gender = '—'
-				break;
-		}
-		if (pokemon.gender) {
-			if (pokemon.gender.indexOf("♂") > -1 || pokemon.gender.toLowerCase() == "m" || pokemon.gender.toLowerCase() == "male")
-				pokemon.gender = "♂"
-			if (pokemon.gender.indexOf("♀") > -1 || pokemon.gender.toLowerCase() == "f" || pokemon.gender.toLowerCase() == "female")
-				pokemon.gender = "♀"
-			if (pokemon.gender.toLowerCase() == "-" || pokemon.gender.toLowerCase() == "none")
-				pokemon.gender = '—'
-		}
-
-		pokemon.amount = this.tryValues(["amount", "count"], entry)
-		pokemon.shiny = this.tryValues(["shiny"], entry)
-		pokemon.nickname = this.tryValues(["nickname"], entry)
-		pokemon.ot = this.tryValues(["ot"], entry)
-		pokemon.tid = this.tryValues(["tid"], entry)
-		pokemon.level = this.tryValues(["level", "lvl", "lv"], entry)
-		pokemon.language = this.tryValues(["language", "lang"], entry)
-		pokemon.notes = this.tryValues(["notes", "note", "comments", "comment"], entry)
-		pokemon.balls = []
-		var balls = this.tryValues(["pokeball", "ball", "pokeballs", "balls"], entry)
-		if (balls) {
-			balls = balls.split(",")
-			for (var i in balls)
-				balls[i] = balls[i].trim()
-			pokemon.balls = balls.filter(e => e)
-		}
-		if (pokemon.balls.length == 0) {
-			if (this.getValue(entry.gsx$poke)) pokemon.balls.push("Poké Ball")
-			if (this.getValue(entry.gsx$great)) pokemon.balls.push("Great Ball")
-			if (this.getValue(entry.gsx$ultra)) pokemon.balls.push("Ultra Ball")
-			if (this.getValue(entry.gsx$master)) pokemon.balls.push("Master Ball")
-			if (this.getValue(entry.gsx$safari)) pokemon.balls.push("Safari Ball")
-			if (this.getValue(entry.gsx$level)) pokemon.balls.push("Level Ball")
-			if (this.getValue(entry.gsx$lure)) pokemon.balls.push("Lure Ball")
-			if (this.getValue(entry.gsx$moon)) pokemon.balls.push("Moon Ball")
-			if (this.getValue(entry.gsx$friend)) pokemon.balls.push("Friend Ball")
-			if (this.getValue(entry.gsx$love)) pokemon.balls.push("Love Ball")
-			if (this.getValue(entry.gsx$heavy)) pokemon.balls.push("Heavy Ball")
-			if (this.getValue(entry.gsx$fast)) pokemon.balls.push("Fast Ball")
-			if (this.getValue(entry.gsx$sport)) pokemon.balls.push("Sport Ball")
-			if (this.getValue(entry.gsx$premier)) pokemon.balls.push("Premier Ball")
-			if (this.getValue(entry.gsx$repeat)) pokemon.balls.push("Repeat Ball")
-			if (this.getValue(entry.gsx$timer)) pokemon.balls.push("Timer Ball")
-			if (this.getValue(entry.gsx$nest)) pokemon.balls.push("Nest Ball")
-			if (this.getValue(entry.gsx$net)) pokemon.balls.push("Net Ball")
-			if (this.getValue(entry.gsx$dive)) pokemon.balls.push("Dive Ball")
-			if (this.getValue(entry.gsx$luxury)) pokemon.balls.push("Luxury Ball")
-			if (this.getValue(entry.gsx$heal)) pokemon.balls.push("Heal Ball")
-			if (this.getValue(entry.gsx$quick)) pokemon.balls.push("Quick Ball")
-			if (this.getValue(entry.gsx$dusk)) pokemon.balls.push("Dusk Ball")
-			if (this.getValue(entry.gsx$cherish)) pokemon.balls.push("Cherish Ball")
-			if (this.getValue(entry.gsx$dream)) pokemon.balls.push("Dream Ball")
-			if (this.getValue(entry.gsx$beast)) pokemon.balls.push("Beast Ball")
-		}
-		if (pokemon.balls.length == 0) { // compatibility with richi3f's sheet
-			if (entry.gsx$_dcgjs) pokemon.balls.push("Poké Ball")
-			if (entry.gsx$_ddv49) pokemon.balls.push("Great Ball")
-			if (entry.gsx$_d415a) pokemon.balls.push("Ultra Ball")
-			if (entry.gsx$_d5fpr) pokemon.balls.push("Master Ball")
-			if (entry.gsx$_d6ua4) pokemon.balls.push("Safari Ball")
-			if (entry.gsx$_d88ul) pokemon.balls.push("Level Ball")
-			if (entry.gsx$_dkvya) pokemon.balls.push("Lure Ball")
-			if (entry.gsx$_dmair) pokemon.balls.push("Moon Ball")
-			if (entry.gsx$_dnp34) pokemon.balls.push("Friend Ball")
-			if (entry.gsx$_dp3nl) pokemon.balls.push("Love Ball")
-			if (entry.gsx$_df9om) pokemon.balls.push("Heavy Ball")
-			if (entry.gsx$_dgo93) pokemon.balls.push("Fast Ball")
-			if (entry.gsx$_di2tg) pokemon.balls.push("Sport Ball")
-			if (entry.gsx$_djhdx) pokemon.balls.push("Premier Ball")
-			if (entry.gsx$_dw4je) pokemon.balls.push("Repeat Ball")
-			if (entry.gsx$_dxj3v) pokemon.balls.push("Timer Ball")
-			if (entry.gsx$_dyxo8) pokemon.balls.push("Nest Ball")
-			if (entry.gsx$_e0c8p) pokemon.balls.push("Net Ball")
-			if (entry.gsx$_dqi9q) pokemon.balls.push("Dive Ball")
-			if (entry.gsx$_drwu7) pokemon.balls.push("Luxury Ball")
-			if (entry.gsx$_dtbek) pokemon.balls.push("Heal Ball")
-			if (entry.gsx$_dupz1) pokemon.balls.push("Quick Ball")
-			if (entry.gsx$_e7d2q) pokemon.balls.push("Dusk Ball")
-			if (entry.gsx$_e8rn7) pokemon.balls.push("Cherish Ball")
-			if (entry.gsx$_ea67k) pokemon.balls.push("Dream Ball")
-			if (entry.gsx$_ebks1) pokemon.balls.push("Beast Ball")
-		}
-		tab.pokemons.push(pokemon)
 	}
 }
